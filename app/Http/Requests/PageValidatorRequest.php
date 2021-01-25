d787a8a3e51ac79a9490553fbc43eed7d1865792
Tim de kok <de.kok.ac@gmail.com>
2021-01-25 16:29:22 +0100

    added validator

---
+++ app/Http/Requests/PageValidatorRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class PageValidatorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if(Auth::user()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'titel' => [
+                'required',
+            ],
+            'page_format' => [
+                'required'
+            ],
+            'content_inhoud' => [
+                'required'
+            ],
+            'Slug' => [
+                'required'
+            ],
+            'nav_format' => [
+                'required'
+            ]
+        ];
+    }
+}
